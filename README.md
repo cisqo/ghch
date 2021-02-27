c1e689192998a7c4b9e3f2eded6f9c9ad2e782c2
sam <sam@freighttrust.com>
2021-02-27 05:41:28 -0800

    feat(1.2.0): release
    fix minor issues and lint

--- README.md
+++ README.md
@@ -8,19 +8,22 @@ This tool it was design to sync multiple github repositories to multiple Clubhou
 
 In order to achieve that, we need a way to define this mapping.
 
-For now, we have `PROJECT_MAPPING_PATH` which represents the path to a json file, which describe the following mapping
+For now, we have `PROJECT_MAPPING_PATH` which represents the path to a json file, which describe the
+following mapping
 
 ```json
 {
-   "githubOrg/repo": "Project"
+  "githubOrg/repo": "Project"
 }
 ```
 
 You'll also need a Github personal access token and a Clubhouse token.
 
-The Github token can be obtained from `Github` > `Settings` > `Developer settings` > `Personal access tokens` > `Generate new token` 
+The Github token can be obtained from `Github` > `Settings` > `Developer settings` >
+`Personal access tokens` > `Generate new token`
 
-For the Clubhouse token, you'll need to go `Clubhouse` > `Settings` > `API Tokens` > `Generate new token`.
+For the Clubhouse token, you'll need to go `Clubhouse` > `Settings` > `API Tokens` >
+`Generate new token`.
 
 ##### Docker
 
@@ -33,12 +36,13 @@ docker run -v ./mapping.json:/opt/gh-clubhouse-sync/project-mapping \
 
 ##### K8s
 
-In order to store the there is a configmap in `chart/templates/configmap.yaml` and the secrets are defined in `values.yaml`.
+In order to store the there is a configmap in `chart/templates/configmap.yaml` and the secrets are
+defined in `values.yaml`.
 
 ```
 helm install --name gh-clubhouse-sync .
 ```
 
-## License 
+## License
 
-MIT 
+MIT
--- import.js
+++ import.js
@@ -1,61 +1,60 @@
-import { map, join, isInteger } from 'lodash'
-import { githubIssueToClubhouseStory } from 'github-clubhouse'
-import { format, subMinutes } from 'date-fns'
-import fs from 'fs'
+import { map, join, isInteger } from 'lodash';
+import { githubIssueToClubhouseStory } from 'github-clubhouse';
+import { format, subMinutes } from 'date-fns';
+import fs from 'fs';
 
 if (!process.env.GITHUB_TOKEN) {
-    console.log('Missing GITHUB_TOKEN env variable')
-    process.exit(1)
+  console.log('Missing GITHUB_TOKEN env variable');
+  process.exit(1);
 }
 
 if (!process.env.CLUBHOUSE_TOKEN) {
-    console.log('Missing CLUBHOUSE_TOKEN env variable')
-    process.exit(1)
+  console.log('Missing CLUBHOUSE_TOKEN env variable');
+  process.exit(1);
 }
 
 if (!process.env.PROJECT_MAPPING_PATH) {
-    console.log('Missing PROJECT_MAPPING_PATH env variable')
-    process.exit(1)
+  console.log('Missing PROJECT_MAPPING_PATH env variable');
+  process.exit(1);
 }
 
-const mappingFile = fs.readFileSync(process.env.PROJECT_MAPPING_PATH)
-const projectMapping = JSON.parse(mappingFile)
+const mappingFile = fs.readFileSync(process.env.PROJECT_MAPPING_PATH);
+const projectMapping = JSON.parse(mappingFile);
 
 function importIssuesFromRepo({ repo, project, query }) {
-    return githubIssueToClubhouseStory({
-        githubToken      : process.env.GITHUB_TOKEN,
-        clubhouseToken   : process.env.CLUBHOUSE_TOKEN,
-        clubhouseProject : project,
-        githubRepo       : repo,
-        query            : query,
-        dryRun           : false,
-        userMap          : "{}"
-    })
+  return githubIssueToClubhouseStory({
+    githubToken: process.env.GITHUB_TOKEN,
+    clubhouseToken: process.env.CLUBHOUSE_TOKEN,
+    clubhouseProject: project,
+    githubRepo: repo,
+    query: query,
+    dryRun: false,
+    userMap: '{}',
+  });
 }
 
 async function importAllIssues(syncInterval) {
-    const query = join([
-      'state:open',
-      createDateFilter(syncInterval)
-    ], ' ')
+  const query = join(['state:open', createDateFilter(syncInterval)], ' ');
 
-    await Promise.all(map(projectMapping, async (project, repo) => (
-        await importIssuesFromRepo({ repo, project, query })
-    )))
+  await Promise.all(
+    map(
+      projectMapping,
+      async (project, repo) => await importIssuesFromRepo({ repo, project, query }),
+    ),
+  );
 
-    console.log('\nSynced.')
+  console.log('\nSynced.');
 }
 
 function createDateFilter(intervalInMinutes) {
-    if (!isInteger(parseInt(intervalInMinutes))) {
-        return null
-    }
-    const createDateLimit = subMinutes(Date.now(), parseInt(intervalInMinutes))
-    return `created:>${format(createDateLimit, "yyyy-MM-dd'T'HH:mm:ssxxx")}`
+  if (!isInteger(parseInt(intervalInMinutes))) {
+    return null;
+  }
+  const createDateLimit = subMinutes(Date.now(), parseInt(intervalInMinutes));
+  return `created:>${format(createDateLimit, "yyyy-MM-dd'T'HH:mm:ssxxx")}`;
 }
 
-importAllIssues(process.env.SYNC_INTERVAL)
-    .catch((e) => {
-        console.log(e)
-        process.exit(1)
-    })
+importAllIssues(process.env.SYNC_INTERVAL).catch((e) => {
+  console.log(e);
+  process.exit(1);
+});
